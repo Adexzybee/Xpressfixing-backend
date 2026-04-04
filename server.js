7ee05a31a238d2e5b6d9ae00c1c2cc8ae2efe790
adexzybee <adexzybee@gmail.com>
2026-04-04 17:15:31 +0100

    COR updated for vercel

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const API_KEY = process.env.API_KEY;
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS 
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:8080'];
+  : ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500', 'https://xpressfixing-frontend.vercel.app'];
 
 app.use(cors({
   origin: function(origin, callback) {
