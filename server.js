053a61d2d01af5f29191c78c2d18255addfa6d69
adexzybee <adexzybee@gmail.com>
2026-04-15 16:27:44 +0100

    added origin

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'xpressfixing_temp_secret_change_in
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS 
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500', 'https://xpressfixing-frontend.vercel.app'];
+  : ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500', 'https://xpressfixing-frontend.vercel.app', 'http://localhost:5173'];
 
 app.use(cors({
   origin: function(origin, callback) {
